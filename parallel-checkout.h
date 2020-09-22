fa33dd99f06e2a6c6ff644fb0967341566317894
Matheus Tavares <matheus.bernardino@usp.br>
2020-09-22 19:49:24 -0300

    unpack-trees: add basic support for parallel checkout
    This new interface allows us to enqueue some of the entries being
    checked out to later call write_entry() for them in parallel. For now,
    the parallel checkout machinery is enabled by default and there is no
    user configuration, but run_parallel_checkout() just writes the queued
    entries in sequence (without spawning additional workers). The next
    patch will actually implement the parallelism and, later, we will make
    it configurable.
    
    When there are path collisions among the entries being written (which
    can happen e.g. with case-sensitive files in case-insensitive file
    systems), the parallel checkout code detects the problem and marks the
    item with PC_ITEM_COLLIDED. Later, these items are sequentially fed to
    checkout_entry() again. This is similar to the way the sequential code
    deals with collisions, overwriting the previously checked out entries
    with the subsequent ones. The only difference is that, when we start
    writing the entries in parallel, we won't be able to determine which of
    the colliding entries will survive on disk (for the sequential
    algorithm, it is always the last one).
    
    I also experimented with the idea of not overwriting colliding entries,
    and it seemed to work well in my simple tests. However, because just one
    entry of each colliding group would be actually written, the others
    would have null lstat() fields on the index. This might not be a problem
    by itself, but it could cause performance penalties for subsequent
    commands that need to refresh the index: when the st_size value cached
    is 0, read-cache.c:ie_modified() will go to the filesystem to see if the
    contents match. As mentioned in the function:
    
        * Immediately after read-tree or update-index --cacheinfo,
        * the length field is zero, as we have never even read the
        * lstat(2) information once, and we cannot trust DATA_CHANGED
        * returned by ie_match_stat() which in turn was returned by
        * ce_match_stat_basic() to signal that the filesize of the
        * blob changed.  We have to actually go to the filesystem to
        * see if the contents match, and if so, should answer "unchanged".
    
    So, if we have N entries in a colliding group and we decide to write and
    lstat() only one of them, every subsequent git-status will have to read,
    convert, and hash the written file N - 1 times, to check that the N - 1
    unwritten entries are dirty. By checking out all colliding entries (like
    the sequential code does), we only pay the overhead once.
    
    Co-authored-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Co-authored-by: Jeff Hostetler <jeffhost@microsoft.com>
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ parallel-checkout.h
@@ -0,0 +1,27 @@
+#ifndef PARALLEL_CHECKOUT_H
+#define PARALLEL_CHECKOUT_H
+
+struct cache_entry;
+struct checkout;
+struct conv_attrs;
+
+enum pc_status {
+	PC_UNINITIALIZED = 0,
+	PC_ACCEPTING_ENTRIES,
+	PC_RUNNING,
+};
+
+enum pc_status parallel_checkout_status(void);
+void init_parallel_checkout(void);
+
+/*
+ * Return -1 if parallel checkout is currently not enabled or if the entry is
+ * not eligible for parallel checkout. Otherwise, enqueue the entry for later
+ * write and return 0.
+ */
+int enqueue_checkout(struct cache_entry *ce, struct conv_attrs *ca);
+
+/* Write all the queued entries, returning 0 on success.*/
+int run_parallel_checkout(struct checkout *state);
+
+#endif /* PARALLEL_CHECKOUT_H */
